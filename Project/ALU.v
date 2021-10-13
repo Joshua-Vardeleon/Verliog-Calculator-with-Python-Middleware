a1de800ccbc9514404b43cb8e5422e5413ab0aa2
Joshua-Vardeleon <Joshua.Vardeleon@gmail.com>
2021-10-12 22:52:04 -0500

    added comments

--- Project/ALU.v
+++ Project/ALU.v
@@ -1,35 +1,44 @@
+//imported needed module
 `include "Decoder.v"
 `include "division_module.v"
 `include "Modulo.v"
 `include "multi.v"
 `include "Multiplexer.v"
+//ALU start, requires 2 16 bit inputs, 4 bit op code and the output of the ALU
 module ALU(inputP, inputQ, opCode, outALU);
 
+//instiantiating inputs and output
 input [15:0] inputP;
 input [15:0] inputQ;
+
 input [3:0] opCode;
 
 output [31:0] outALU;
 
-
+//these inputs are used to connect arithmetic outputs into the multiplexer
 input [31:0] outMod;
 
 input [31:0] outDiv;
 
 input [31:0] outMult;
 
+//outputs for the error module
 output [1:0] divZeroMod;
 
 output [1:0] divZeroDiv;
 
+//for the decoder
 input [15:0] hotselect;
 
-
-Modulo inst1(inputP, inputQ, outMod, divZeroMod);
-Division inst2(inputP, inputQ, outDiv, divZeroDiv);
-Multiplication inst3(inputP, inputQ, outMult);
+//calling all modules
+Modulo inst1(inputP, inputQ, outMod, divZeroMod); //calculating modulo outputs the results (outMod) and a divide by zero 2 bit code
+Division inst2(inputP, inputQ, outDiv, divZeroDiv); //calculating division outputs the results (outDiv) and a divide by zero 2 bit code
+Multiplication inst3(inputP, inputQ, outMult); //calculating multiplication outputs the results (outMult)
 Decoder inst4(opCode, hotselect);
-Multiplexer inst5(outMult, outDiv, outMod, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, hotselect, outALU);
+
+
+Multiplexer inst5(outMult, outDiv, outMod, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
+32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, hotselect, outALU); //multiplexer, for opcodes multiplication 0000, division 0001, Modulus 0010
 
 
  endmodule
@@ -44,9 +53,9 @@ module testbench();
   ALU electricity1(inputP, inputQ, opCode, outALU); // Declare 'multiplication' module
 
 	initial begin
-		// for ints < 250
-		assign inputP = 16'b11111;	// # 210
-		assign inputQ = 16'b0011;	// # 100
+		//
+		assign inputP = 16'b11111;	// # 31
+		assign inputQ = 16'b0011;	// # 3
         assign opCode = 4'b0000;
 		
 		#30;
